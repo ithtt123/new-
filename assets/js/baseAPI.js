3699e821bd1499bf50837f67244a6c6a303a1245
htt <3050783279@qq.com>
2022-09-29 15:51:15 +0800

    完成了登录和注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,25 +1,22 @@
 $.ajaxPrefilter(function (options) {
+  // 将键值对转换JSON字符串格式
+  const format2Json = (source) => {
+    let target = {}
+    source.split('&').forEach(el => {
+      let kv = el.split('=')
+      target[kv[0]] = kv[1]
+    })
+    return JSON.stringify(target)
+  }
   // 统一设置根路径
-  // options.url = 'http://www.liulongbin.top:3007' + options.url
-  // options.url = 'http://www.liulongbin.top:3008' + options.url
-  options.url = 'http://big-event-api-t.itheima.net' + options.url
-  // console.log(options.url)
-  // 统一设置headers请求头
-  if (options.url.indexOf('/my/') !== -1) {
-    options.headers = {
-      Authorization: localStorage.getItem('token') || ''
-    }
-  }
+  options.url = 'http://big-event-vue-api-t.itheima.net' + options.url
 
-  // 全局统一挂载complete无论成功还是失败都会调用这个函数
-  options.complete = function (res) {
-    // console.log(res)
-    // responseJSON可以拿到服务器响应的数据
-    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-      // 强制清空本地存储toktn
-      localStorage.removeItem('token')
-      // 强制跳转页面到login
-      location.href = 'login.html'
-    }
-  }
+  // 统一设置headers请求头
+  options.contentType = 'application/json'
+
+  // 将键值对转换JSON字符串格式
+  options.data = format2Json(options.data)
+
+
+
 })
